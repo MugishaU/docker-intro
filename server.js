d218c741d1a4097fcbee0be17bdb633f889ebe54
MugishaU <muwiragiye123@gmail.com>
2022-11-17 20:52:40 +0000

    add log to each API call

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 const express = require("express")
 
 // Constants
-const PORT = 8080
+const PORT = "8080"
 const HOST = "0.0.0.0"
 
 // App
 const app = express()
-app.get("/", (req, res) => {
-	res.send({ response: "Hello World" })
+app.get("/name", (req, res) => {
+	console.log(`The app returned information at ${new Date()}`)
+	res.send({ name: "John" })
 })
 
 app.listen(PORT, HOST, () => {
